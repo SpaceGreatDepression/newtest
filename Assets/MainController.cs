4cb3893aa53c2c4d290e9e30591cd7b73d4e7548
space <qkdnrh10@yfactorysoft.com>
2017-08-07 00:18:53 +0900

    asdasd

--- Assets/MainController.cs
+++ Assets/MainController.cs
@@ -4,10 +4,57 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class MainController : MonoBehaviour {
+	public ToggleController TC;
+	public void result(){
+		if (TC.PowerToggle.T.transform.GetChild (0).gameObject.activeSelf) {
+			if (TC.PowerToggle.S.text != "") {
+				TC.NomalToggle [0].T.transform.GetChild (0).gameObject.SetActive (true);
+				TC.NomalToggle [2].T.transform.GetChild (0).gameObject.SetActive (true);
+				TC.NomalToggle [1].T.transform.GetChild (0).gameObject.SetActive (false);
+				TC.NomalToggle [3].T.transform.GetChild (0).gameObject.SetActive (false);
 
-public LoopHorizontalScrollRect LoopHorizontal;
 
-public void CreateScrollrect(){
-//LoopHorizontal.ref
-}
+			}
+
+
+		} else {
+			if (TC.maker.Count == 2) {
+				if (TC.maker [0].S.text != "" && TC.maker [1].S.text != "") {
+				
+					if ((pp (TC.maker [0].T).name == "2" && pp (TC.maker [1].T).name == "3") || (pp (TC.maker [1].T).name == "2" && pp (TC.maker [0].T).name == "3")) {
+					    //가로세로
+						float g ,s,d,ga;
+						if (pp (TC.maker [0].T).name == "2") {
+							g = float.Parse (TC.maker [0].S.text);
+							s = float.Parse (TC.maker [1].S.text);
+						} else {
+							g = float.Parse (TC.maker [1].S.text);
+							s = float.Parse (TC.maker [0].S.text);
+						}
+						d = Mathf.Sqrt (Mathf.Pow(g,2)+Mathf.Pow(s,2));
+						ga = Mathf.Atan(s/g);
+						Debug.Log (ga);
+						Debug.Log (ga*Mathf.Rad2Deg);
+					//	TC.NomalToggle [2].S.text
+
+					}else if((pp (TC.maker [0].T).name == "2" && pp (TC.maker [1].T).name == "4") || (pp (TC.maker [1].T).name == "2" && pp (TC.maker [0].T).name == "4")){
+						//가로대각
+					}else if((pp (TC.maker [0].T).name == "2" && pp (TC.maker [1].T).name == "5") || (pp (TC.maker [1].T).name == "2" && pp (TC.maker [0].T).name == "5")){
+						//가로각도
+					}else if((pp (TC.maker [0].T).name == "3" && pp (TC.maker [1].T).name == "4") || (pp (TC.maker [1].T).name == "3" && pp (TC.maker [0].T).name == "4")){
+						//세로대각
+					}else if((pp (TC.maker [0].T).name == "3" && pp (TC.maker [1].T).name == "5") || (pp (TC.maker [1].T).name == "3" && pp (TC.maker [0].T).name == "5")){
+						//세로각도
+					}else if((pp (TC.maker [0].T).name == "4" && pp (TC.maker [1].T).name == "5") || (pp (TC.maker [1].T).name == "4" && pp (TC.maker [0].T).name == "5")){
+						//대각각도
+					}
+
+
+				}
+			}
+		}
+	}
+	Transform pp(GameObject g){
+		return g.transform.parent.parent;
+	}
 }
--- Assets/ToggleController.cs
+++ Assets/ToggleController.cs
@@ -1,16 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ToggleController : MonoBehaviour {
 
+	public TS PowerToggle;
+	public List<TS> NomalToggle = new List<TS>();
+	public List<TS> maker = new List<TS> ();
+
 	// Use this for initialization
-	void Start () {
-		
-	}
+
 	
 	// Update is called once per frame
-	void Update () {
-		
+	GameObject saveT;
+	public void Togglecheck(GameObject T){
+		saveTb = true;
+		if (T == PowerToggle.T) {
+			//saveT = null;
+			ToggleCallback (PowerToggle);
+		} else {
+
+				for (int i = 0; i < NomalToggle.Count; i++) {
+					//Debug.Log (T.GetInstanceID());
+					//Debug.Log (NomalToggle [i].T.GetInstanceID());
+					if (NomalToggle [i].T == T) {
+						
+						ToggleCallback (NomalToggle [i]);
+					if (saveTb) {
+						saveT = T;
+					}
+						break;
+					}
+				}
+
+
+		}
+	}
+	bool saveTb = true;
+	public void ToggleCallback(TS ts){
+		if (ts == PowerToggle) {
+			if (PowerToggle.T.transform.GetChild (0).gameObject.activeSelf) {
+				PowerToggle.T.transform.GetChild (0).gameObject.SetActive (false);
+			} else {
+				PowerToggle.T.transform.GetChild (0).gameObject.SetActive (true);
+			}
+		} else {
+			if (saveT == ts.T) {
+				maker [maker.Count - 1].T.transform.GetChild (0).gameObject.SetActive (false);
+				maker.RemoveAt (maker.Count - 1);
+				saveT = null;
+				saveTb = false;
+			} else {
+				ts.T.transform.GetChild (0).gameObject.SetActive (true);
+				;
+				if (maker.Count < 2) {
+					maker.Add (ts);
+				} else {
+					maker [0].T.transform.GetChild (0).gameObject.SetActive (false);
+					maker.RemoveAt (0);
+
+					maker.Add (ts);
+				}
+			}
+		}
+	}
+	[System.Serializable]
+	public class TS{
+		public GameObject T;
+		public Text S;
+
+		public TS(GameObject t,Text s){
+			T = t;
+			S = s;
+		}
 	}
 }
